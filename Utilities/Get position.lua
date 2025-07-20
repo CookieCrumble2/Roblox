c23254f4375de62e890c5cd181d14ef415d65949
CookieCrumble <168351007+CookieCrumble2@users.noreply.github.com>
2025-07-19 23:32:41 -0400

    Create Get position.lua

---
+++ Utilities/Get position.lua
@@ -0,0 +1,9 @@
+local type = false
+local p = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+
+if type then
+    print(p)
+else
+    setclipboard(tostring(p))
+end
+--[[Those who know💀]]--
